ead6c3a60aa057902f83f609a36b0200f3a4420d
William Brockhus <pickeringw@gmail.com>
2019-08-06 13:32:59 +1000

    Remove node_modules from build

--- bin/build.js
+++ bin/build.js
@@ -4,7 +4,6 @@ var fs = require('fs-extra');
 
 rimraf.sync(path.resolve(path.dirname(__dirname), 'app'));
 fs.copySync(path.resolve(path.dirname(__dirname), 'src/main/lib'), path.resolve(path.dirname(__dirname), 'app', 'lib'));
-fs.copySync(path.resolve(path.dirname(__dirname), 'src/main/node_modules'), path.resolve(path.dirname(__dirname), 'app', 'node_modules'));
 fs.copySync(path.resolve(path.dirname(__dirname), 'src/main/resources'), path.resolve(path.dirname(__dirname), 'app', 'resources'));
 fs.copySync(path.resolve(path.dirname(__dirname), 'src/main/index.js'), path.resolve(path.dirname(__dirname), 'app', 'index.js'));
 fs.copySync(path.resolve(path.dirname(__dirname), 'src/main/index.html'), path.resolve(path.dirname(__dirname), 'app', 'index.html'));
