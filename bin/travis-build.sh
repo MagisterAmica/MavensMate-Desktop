9618f94389c16dfa1be91130a3e67310c840c182
William Brockhus <pickeringw@gmail.com>
2019-08-08 15:59:51 +1000

    Update travis build script

--- bin/travis-build.sh
+++ bin/travis-build.sh
@@ -4,10 +4,15 @@ set -e # Automatic exit on any failure
 set -x # Print commands after they are expanded but before they are executed
 
 OS="${TRAVIS_OS_NAME}"
+ACTION="build"
 
 if [ -z "${OS}" ]; then
   echo "OS is unset, defaulting to linux"
   OS="linux"
+fi
+
+if ! [ -z "${GH_TOKEN}" ]; then
+  ACTION="dist"
 fi
 
 node -v
@@ -17,14 +22,14 @@ yarn install
 
 if [ "${OS}" = "linux" ]; then
   echo "running linux build"
-  yarn publish
+  yarn "${ACTION}"
 elif [ "${OS}" = "osx" ]; then
   echo "running osx build"
   security find-identity -v -p codesigning
-  yarn publish
+  yarn "${ACTION}"
 elif [ "${OS}" = "windows" ]; then
   echo "running windows build"
-  yarn publish
+  yarn "${ACTION}"
 fi
 
 ls dist
