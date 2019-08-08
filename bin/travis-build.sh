70efe8a16bd6c843f0611defbd309156b7b3653c
William Brockhus <pickeringw@gmail.com>
2019-08-08 16:11:42 +1000

    Prevent echoing GH_TOKEN

--- bin/travis-build.sh
+++ bin/travis-build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
+ACTION="build"
+if ! [ -z "${GH_TOKEN}" ]; then
+  ACTION="dist"
+fi
+
 set -e # Automatic exit on any failure
 set -x # Print commands after they are expanded but before they are executed
 
 OS="${TRAVIS_OS_NAME}"
-ACTION="build"
 
 if [ -z "${OS}" ]; then
   echo "OS is unset, defaulting to linux"
   OS="linux"
-fi
-
-if ! [ -z "${GH_TOKEN}" ]; then
-  ACTION="dist"
 fi
 
 node -v
